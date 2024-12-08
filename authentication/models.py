219b1b1a623c332b1ceb353b3437b7ad3f3e1c27
buriihenry <buriihenry@gmail.com>
2024-12-08 09:37:44 +0300

    code to handle email verification

--- authentication/models.py
+++ authentication/models.py
@@ -4,5 +4,6 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
+    is_email_verified = models.BooleanField(default=False)    
     def __str__(self):
         return self.email
--- todosite/settings.py
+++ todosite/settings.py
@@ -117,6 +117,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+#email config
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_FROM_USER = os.environ.get('DEFAULT_FROM_EMAIL')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
